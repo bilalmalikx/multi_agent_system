e4faba906c5f24bd5da47709fe87526f9c75225c
Bilal Siddique <outlookbilalmalik@gmail.com>
2026-03-17 22:59:03 +0500

    Set the llm to call it dynamiccaly in every agent files

---
+++ app/core/tracing.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+from app.core.settings import settings
+
+def get_llm():
+    llm = ChatOpenAI(
+        model="gpt-4-0613",
+        temperature=0.7,
+        openai_api_key=settings.OPENAI_API_KEY,
+        tracing_v2=settings.LANGSMITH_TRACING_V2,
+        tracing_project=settings.LANGSMITH_PROJECT,
+    )
+    return llm
